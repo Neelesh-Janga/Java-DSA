53566a1d234cd7c9a6f42e2fc726c4a7959c2201
Neelesh-Janga <codelikeneelesh@gmail.com>
2023-11-27 21:35:59 +0530

    Singly Linked List

---
+++ src/main/java/neelesh/data_structures/linkedlist/nodes/SinglyLinkedListNode.java
@@ -0,0 +1,11 @@
+package main.java.neelesh.data_structures.linkedlist.nodes;
+
+public class SinglyLinkedListNode {
+    public int val;
+    public SinglyLinkedListNode next;
+
+    public SinglyLinkedListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
